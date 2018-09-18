2b71e9711375e7f37958457428ac6a43714e98e6
张明强 <zhangmqa@jiedaibao.com>
2018-09-18 15:44:16 +0800

    update

--- src/components/Foo.vue
+++ src/components/Foo.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="wrap">
       <h4>引入scss</h4>
-      <p class="item">
-        1、安装node-sass、sass-loader<br>
-        <img src="../assets/install-scss.png">
-      </p>
       <p>
-        2、不需要配置任何东西，直接在style标签添加  lang="scss"  就可以使用
+        不需要配置任何东西，直接在style标签添加  lang="scss"  就可以使用
+      </p>
+      <h4>路由配置</h4>
+      <p>
+        子路由path的值不要加‘/’，否则会跳转到根路由
       </p>
     </div>
 </template>
